9ccb32c484dd84c3a32f88067efca5383679f200
Emon526 <emonats526@gmail.com>
2023-10-16 04:37:31 +0600

    custom Button Added

---
+++ lib/widgets/cutombutton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../consts/consts.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.ontap,
+    required this.buttontext,
+  });
+  final Function()? ontap;
+  final String buttontext;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Consts.primaryColor,
+      borderRadius: BorderRadius.circular(Consts.defaultBorderRadius),
+      child: InkWell(
+        onTap: ontap,
+        borderRadius: BorderRadius.circular(Consts.defaultBorderRadius),
+        child: Center(
+          heightFactor: 3,
+          child: Text(buttontext,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+      ),
+    );
+  }
+}
